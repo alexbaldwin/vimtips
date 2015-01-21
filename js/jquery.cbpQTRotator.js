be1581cb672a47826bf6eb36d88056913014ddec
Alex Baldwin <alexbaldwin@gmail.com>
2015-01-20 21:25:30 -0800

    Wildcard

--- js/jquery.cbpQTRotator.js
+++ js/jquery.cbpQTRotator.js
@@ -10,148 +10,152 @@
  */
 ;( function( $, window, undefined ) {
 
-	'use strict';
+  'use strict';
 
-	// global
-	var Modernizr = window.Modernizr;
+  // global
+  var Modernizr = window.Modernizr;
 
-	$.CBPQTRotator = function( options, element ) {
-		this.$el = $( element );
-		this._init( options );
-	};
+  $.CBPQTRotator = function( options, element ) {
+    this.$el = $( element );
+    this._init( options );
+  };
 
-	// the options
-	$.CBPQTRotator.defaults = {
-		// default transition speed (ms)
-		speed : 700,
-		// default transition easing
-		easing : 'ease',
-		// rotator interval (ms)
-		interval : 8000
-	};
+  // the options
+  $.CBPQTRotator.defaults = {
+    // default transition speed (ms)
+    speed : 700,
+    // default transition easing
+    easing : 'ease',
+    // rotator interval (ms)
+    interval : 8000
+  };
 
-	$.CBPQTRotator.prototype = {
-		_init : function( options ) {
+  $.CBPQTRotator.prototype = {
+    _init : function( options ) {
 
-			// options
-			this.options = $.extend( true, {}, $.CBPQTRotator.defaults, options );
-			// cache some elements and initialize some variables
-			this._config();
-			// show current item
-			this.$items.eq( this.current ).addClass( 'cbp-qtcurrent' );
-			// set the transition to the items
-			if( this.support ) {
-				this._setTransition();
-			}
-			// start rotating the items
-			this._startRotator();
+      // options
+      this.options = $.extend( true, {}, $.CBPQTRotator.defaults, options );
+      // cache some elements and initialize some variables
+      this._config();
+      // show current item
+      this.$items.eq( this.current ).addClass( 'cbp-qtcurrent' );
+      // set the transition to the items
 
-		},
-		_config : function() {
 
-			// the content items
-			this.$items = this.$el.children( 'div.cbp-qtcontent' );
-			// total items
-			this.itemsCount = this.$items.length;
-			// current item's index
-			this.current = 0;
-			// support for CSS Transitions
-			this.support = Modernizr.csstransitions;
-			// add the progress bar
-			if( this.support ) {
-				this.$progress = $( '<span class="cbp-qtprogress"></span>' ).appendTo( this.$el );
-			}
+      if( this.support ) {
+        this._setTransition();
+      }
+      // start rotating the items
+      this._startRotator();
 
-		},
-		_setTransition : function() {
-			setTimeout( $.proxy( function() {
-				this.$items.css( 'transition', 'opacity ' + this.options.speed + 'ms ' + this.options.easing );
-			}, this ), 25 );
-		},
-		_startRotator: function() {
+    },
+    _config : function() {
 
-			if( this.support ) {
-				this._startProgress();
-			}
+      // the content items
+      this.$items = this.$el.children( 'div.cbp-qtcontent' );
+      // total items
+      this.itemsCount = this.$items.length;
+      // current item's index
+      this.current = Math.floor(Math.random() * this.$items.length);
+      // support for CSS Transitions
+      this.support = Modernizr.csstransitions;
+      // add the progress bar
+      if( this.support ) {
+        this.$progress = $( '<span class="cbp-qtprogress"></span>' ).appendTo( this.$el );
+      }
 
-			setTimeout( $.proxy( function() {
-				if( this.support ) {
-					this._resetProgress();
-				}
-				this._next();
-				this._startRotator();
-			}, this ), this.options.interval );
+    },
+    _setTransition : function() {
+      setTimeout( $.proxy( function() {
+        this.$items.css( 'transition', 'opacity ' + this.options.speed + 'ms ' + this.options.easing );
+      }, this ), 25 );
+    },
+    _startRotator: function() {
 
-		},
-		_next : function() {
+      if( this.support ) {
+        this._startProgress();
+      }
 
-			// hide previous item
-			this.$items.eq( this.current ).removeClass( 'cbp-qtcurrent' );
-			// update current value
-			this.current = this.current < this.itemsCount - 1 ? this.current + 1 : 0;
-			// show next item
-			this.$items.eq( this.current ).addClass('cbp-qtcurrent');
+      setTimeout( $.proxy( function() {
+        if( this.support ) {
+          this._resetProgress();
+        }
+        this._next();
+        this._startRotator();
+      }, this ), this.options.interval );
 
-		},
-		_startProgress : function() {
-			
-			setTimeout( $.proxy( function() {
-				this.$progress.css( { transition : 'width ' + this.options.interval + 'ms linear', width : '100%' } );
-			}, this ), 25 );
+    },
+    _next : function() {
 
-		},
-		_resetProgress : function() {
-			this.$progress.css( { transition : 'none', width : '0%' } );
-		},
-		destroy : function() {
-			if( this.support ) {
-				this.$items.css( 'transition', 'none' );
-				this.$progress.remove();
-			}
-			this.$items.removeClass( 'cbp-qtcurrent' ).css( {
-				'position' : 'relative',
-				'z-index' : 100,
-				'pointer-events' : 'auto',
-				'opacity' : 1
-			} );
-		}
-	};
+      // hide previous item
+      this.$items.eq( this.current ).removeClass( 'cbp-qtcurrent' );
+      // update current value
+      // this.current = this.current < this.itemsCount - 1 ? this.current + 1 : 0;
+      // randomize value
+      this.current = Math.floor(Math.random() * this.itemsCount);
+      // show next item
+      this.$items.eq( this.current ).addClass('cbp-qtcurrent');
 
-	var logError = function( message ) {
-		if ( window.console ) {
-			window.console.error( message );
-		}
-	};
+    },
+    _startProgress : function() {
 
-	$.fn.cbpQTRotator = function( options ) {
-		if ( typeof options === 'string' ) {
-			var args = Array.prototype.slice.call( arguments, 1 );
-			this.each(function() {
-				var instance = $.data( this, 'cbpQTRotator' );
-				if ( !instance ) {
-					logError( "cannot call methods on cbpQTRotator prior to initialization; " +
-					"attempted to call method '" + options + "'" );
-					return;
-				}
-				if ( !$.isFunction( instance[options] ) || options.charAt(0) === "_" ) {
-					logError( "no such method '" + options + "' for cbpQTRotator instance" );
-					return;
-				}
-				instance[ options ].apply( instance, args );
-			});
-		} 
-		else {
-			this.each(function() {	
-				var instance = $.data( this, 'cbpQTRotator' );
-				if ( instance ) {
-					instance._init();
-				}
-				else {
-					instance = $.data( this, 'cbpQTRotator', new $.CBPQTRotator( options, this ) );
-				}
-			});
-		}
-		return this;
-	};
+      setTimeout( $.proxy( function() {
+        this.$progress.css( { transition : 'width ' + this.options.interval + 'ms linear', width : '100%' } );
+      }, this ), 25 );
+
+    },
+    _resetProgress : function() {
+      this.$progress.css( { transition : 'none', width : '0%' } );
+    },
+    destroy : function() {
+      if( this.support ) {
+        this.$items.css( 'transition', 'none' );
+        this.$progress.remove();
+      }
+      this.$items.removeClass( 'cbp-qtcurrent' ).css( {
+        'position' : 'relative',
+        'z-index' : 100,
+        'pointer-events' : 'auto',
+        'opacity' : 1
+      } );
+    }
+  };
+
+  var logError = function( message ) {
+    if ( window.console ) {
+      window.console.error( message );
+    }
+  };
+
+  $.fn.cbpQTRotator = function( options ) {
+    if ( typeof options === 'string' ) {
+      var args = Array.prototype.slice.call( arguments, 1 );
+      this.each(function() {
+        var instance = $.data( this, 'cbpQTRotator' );
+        if ( !instance ) {
+          logError( "cannot call methods on cbpQTRotator prior to initialization; " +
+                   "attempted to call method '" + options + "'" );
+          return;
+        }
+        if ( !$.isFunction( instance[options] ) || options.charAt(0) === "_" ) {
+          logError( "no such method '" + options + "' for cbpQTRotator instance" );
+          return;
+        }
+        instance[ options ].apply( instance, args );
+      });
+    } 
+    else {
+      this.each(function() {	
+        var instance = $.data( this, 'cbpQTRotator' );
+        if ( instance ) {
+          instance._init();
+        }
+        else {
+          instance = $.data( this, 'cbpQTRotator', new $.CBPQTRotator( options, this ) );
+        }
+      });
+    }
+    return this;
+  };
 
 } )( jQuery, window );
